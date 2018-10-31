3712a0d293916c9eb5762d5ef438a91bf8e94c55
DFP <dfpcloud@gmail.com>
2018-10-31 12:33:49 +0530

    Update gitjenkins.sh

--- gitjenkins.sh
+++ gitjenkins.sh
@@ -5,7 +5,8 @@ sudo apt-get install openjdk-8-jre openjdk-8-jre-headless openjdk-8-jdk -y
 sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update -y
-sudo apt-get install jenkins -y
+#sudo apt-get install jenkins -y
+sudo apt-get install jenkins=2.121.1 -y
 sudo systemctl start jenkins
 while [ ! -f /var/lib/jenkins/secrets/initialAdminPassword ]
 do
