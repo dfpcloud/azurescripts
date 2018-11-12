3eb5fc5c6cdf163a7253970710e9135ee9764a76
DFP <dfpcloud@gmail.com>
2018-11-12 15:53:50 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install nodejs -y
-sudo apt update
+sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
+sudo apt update -y
+apt-cache policy docker-ce -y
 sudo apt install docker-ce -y
+sudo usermod -aG docker $1
 sudo docker -v
 while [ $? != 0 ]
 do
