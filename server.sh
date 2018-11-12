ef2a9033743912f45c39d6433ffeb08c3e60d996
DFP <dfpcloud@gmail.com>
2018-11-12 17:10:28 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -8,8 +8,6 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update -y
 apt-cache policy docker-ce -y
 sudo apt install docker-ce -y
-a=$(id -g)
-sudo newgrp docker
-sudo newgrp $a
-
+sudo usermod -aG docker testadmin
+newgrp docker
 
